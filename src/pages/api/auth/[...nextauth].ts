864f634da830f6b0946e1c75615aba33e7e5ce53
Roy Henriquez <roy.henriquez@gmail.com>
2022-08-22 09:41:51 -0500

    implemented refresh token path

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,54 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { exchangeCodeForAccessToken, exchangeNpssoForCode } from "psn-api";
+import {
+  exchangeCodeForAccessToken,
+  exchangeNpssoForCode,
+  exchangeRefreshTokenForAuthTokens,
+} from "psn-api";
+
+async function refreshAccessToken(token: JWT): Promise<JWT> {
+  try {
+    const authorization = await exchangeRefreshTokenForAuthTokens(
+      token.authorization.refreshToken
+    );
+
+    return {
+      ...token,
+      authorization,
+      accessTokenExpiresIn: Date.now() + authorization.expiresIn * 1000,
+    };
+  } catch (error) {
+    console.log(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
 
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    jwt({ token, user }) {
+    async jwt({ token, user }) {
       if (user) {
         token.authorization = user.authorization;
+        token.accessTokenExpiresIn =
+          Date.now() + token.authorization.expiresIn * 1000;
       }
-      return token;
+
+      if (Date.now() < token.accessTokenExpiresIn) {
+        return token;
+      }
+
+      return refreshAccessToken(token);
     },
     async session({ session, token }) {
       if (token) {
         session.authorization = token.authorization;
-        session.authPayload = { accessToken: token.authorization.accessToken };
+        session.authPayload = {
+          accessToken: session.authorization.accessToken,
+        };
       }
       return session;
     },
@@ -23,7 +58,7 @@ export const authOptions: NextAuthOptions = {
       credentials: {
         npsso: { label: "NPSSO", type: "text" },
       },
-      async authorize(credentials, req) {
+      async authorize(credentials) {
         if (!credentials?.npsso) {
           return null;
         }
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -19,5 +19,6 @@ declare module "next-auth/jwt" {
   /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
   interface JWT {
     authorization: AuthTokensResponse;
+    accessTokenExpiresIn: number;
   }
 }
